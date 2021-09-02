e29d6f87ca002608c506a8bb0907c6fb2002e763
Sanchit <sanchit0496@gmail.com>
2021-09-03 00:55:21 +0530

    updating

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,27 +1,36 @@
 import {useQuery, gql} from "@apollo/client";
 
 const getBooks = gql`
-  {
-    books {
-      name
+    {
+        books{
+            name
+        }
     }
-  }
 `;
 
+
 function BookList() {
-  const {loading, error, data} = useQuery(getBooks);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error </p>;
-  console.log(data);
-  return (
-    <div>
-      <ul id="book-list">
-        {data.books.map(book => (
-          <li key={book.id}>{book.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    const{loading, error, data} = useQuery(getBooks)
+    if(loading){
+        return <p>Loading</p>
+    }
+    if(error){
+        return <p>Error</p>
+    }
+
+
+    return (
+        <div>
+            <ul id="book-list">
+
+            </ul>
+            {
+                data.books.map((book) => {
+                    return <li>{book.name}</li>
+                })
+            }
+        </div>
+    );
 }
 
 export default BookList;
