a890ccae6d627dcf2c13cf5d873326884551b014
Sanchit <sanchit0496@gmail.com>
2021-09-02 09:44:38 +0530

    the mongodb now connected

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+
 
 const app = express();
+
+
+mongoose.connect('mongodb+srv://test:test@cluster0.w6jru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+    console.log('db connected')
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -74,6 +74,12 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args){
                 return books;
             }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args){
+                return authors;
+            }
         }
     }
 })
